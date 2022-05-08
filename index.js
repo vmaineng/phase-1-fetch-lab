bf4e7683d79a95720fbb944406f326d7d2aa085d
Mai Vang <vmaineng@gmail.com>
2022-05-08 15:33:27 -0700

    commit tree 7583f26a6196f9862ef3c7938486e8609834c5b9

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then( r => r.json() )
+  .then( books => {
+     renderBooks( books)
+})
   
-}
+};
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+
+// const renderBooks = books => {}
+
+const renderBooks = books => books.forEach( renderSingle); 
+
+const renderSingle = book => {
+    const main = document.querySelector('main');
+    const liElement = document.createElement('li');
+    liElement.textContent = book.name;
+    main.appendChild(liElement);
+  };
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
